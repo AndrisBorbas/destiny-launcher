ea76bee7e6c4e1b2cd299000e660da750ba3a145
Andris Borbás <andris.borbas@gmail.com>
2021-03-08 19:18:01 +0100

    Add google analytics to measure page views

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,41 @@ import "@/styles/components.scss";
 import "@/styles/utilities.css";
 import "@/styles/global.scss";
 
-import type { AppProps } from "next/app";
+import type { AppProps, NextWebVitalsMetric } from "next/app";
 import React from "react";
 
+import * as gtag from "@/utils/gtag";
+import { useRouter } from "next/dist/client/router";
+
+export function reportWebVitals({
+	id,
+	name,
+	label,
+	value,
+}: NextWebVitalsMetric) {
+	// Use `window.gtag` if you initialized Google Analytics as this example:
+	// https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics/pages/_document.js
+	window.gtag("event", name, {
+		event_category:
+			label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
+		value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
+		event_label: id, // id unique to current page load
+		non_interaction: true, // avoids affecting bounce rate.
+	});
+}
+
 export default function DLApp({ Component, pageProps }: AppProps) {
+	const router = useRouter();
+	React.useEffect(() => {
+		const handleRouteChange = (url: string) => {
+			gtag.pageview(url);
+		};
+		router.events.on("routeChangeComplete", handleRouteChange);
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
+
 	return (
 		<React.StrictMode>
 			<Component {...pageProps} />
