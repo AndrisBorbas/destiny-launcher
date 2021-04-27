21de5257b542cc69641ed9fa67aecb52788e23fa
Andris Borbás <andris.borbas@gmail.com>
2021-04-27 02:14:50 +0200

    Add new sites and notices

---
+++ src/components/notice/Notice.tsx
@@ -0,0 +1,42 @@
+import clsx from "clsx";
+import React, { HTMLProps, useEffect, useState } from "react";
+import { FaTimes } from "react-icons/fa";
+
+import styles from "./Notice.module.scss";
+
+type NoticeProps = {
+	id: string;
+} & HTMLProps<HTMLDivElement>;
+
+export default function Notice({ id, children }: NoticeProps) {
+	const [isVisible, toggleVisible] = useState(false);
+
+	useEffect(() => {
+		const jsonString = localStorage.getItem(id);
+		const value = jsonString != null ? JSON.parse(jsonString) : true;
+		toggleVisible(value);
+		return () => {};
+	}, [id]);
+	return (
+		<div style={{ display: isVisible === true ? "" : "none" }} className="mb-4">
+			<h3
+				className={clsx(
+					styles.notice,
+					"relative mx-auto p-6 px-9 text-center text-xl bg-button border-t border-gray-300",
+				)}
+			>
+				{children}
+				<button
+					className="absolute right-1 top-1 p-1"
+					type="button"
+					onClick={() => {
+						localStorage.setItem(id, JSON.stringify(false));
+						toggleVisible(false);
+					}}
+				>
+					<FaTimes />
+				</button>
+			</h3>
+		</div>
+	);
+}
