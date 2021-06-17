791afb76c36dec498177cbcb9fbe101487b12442
Andris Borbás <andris.borbas@gmail.com>
2021-06-17 04:29:55 +0200

    Remove login page

--- src/components/faq/FAQ.tsx
+++ src/components/faq/FAQ.tsx
@@ -4,6 +4,8 @@ import FAQContent from "@/data/FAQ.mdx";
 
 import styles from "./FAQ.module.scss";
 
+/* eslint-disable react/display-name */
+
 const components = {
 	// eslint-disable-next-line jsx-a11y/heading-has-content
 	h3: ({ ...restProps }) => <h3 className={styles.question} {...restProps} />,
@@ -14,6 +16,8 @@ const components = {
 		<a className={styles.link} rel="noopener" target="_blank" {...restProps} />
 	),
 };
+
+/* eslint-enable react/display-name */
 
 export default function FAQ() {
 	return (
