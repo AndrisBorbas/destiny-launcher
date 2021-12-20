f2ac687dc20d8fbbe45fa393bfb6eb446779e36d
Andris Borbás <andris.borbas@gmail.com>
2021-12-20 23:22:08 +0100

    Fix error

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -4,7 +4,7 @@ import clsx from "clsx";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
-import React, { useEffect, useState } from "react";
+import { forwardRef, memo, useEffect, useState } from "react";
 import { FaAngleDown, FaGripVertical } from "react-icons/fa";
 
 import { currentCharacter, getPlatformCode } from "@/utils/bungieApi/utils";
@@ -14,6 +14,7 @@ import styles from "./Banner.module.scss";
 import H4 from "./H4";
 
 type BannerProps = {
+	/* eslint-disable react/no-unused-prop-types */
 	iconSrc: string;
 	headerText: string;
 	previewImage: string;
@@ -24,191 +25,188 @@ type BannerProps = {
 	id: string;
 	isActive?: boolean;
 	children: MDXRemoteSerializeResult;
+	/* eslint-enable react/no-unused-prop-types */
 } & React.HTMLProps<HTMLDivElement>;
 
-const Banner = React.memo(
-	React.forwardRef<HTMLDivElement, BannerProps>(
-		(
-			/* eslint-disable react/prop-types */
-			{
-				iconSrc,
-				headerText,
-				previewImage,
-				previewBlurhash,
-				url,
-				category,
-				loggedInURL,
-				id,
-				isActive,
-				children,
-			},
-			/* eslint-enable react/prop-types */
-			ref,
-		) => {
-			// Banners are hydrated with them being closed, less layout shift
-			const [isOpened, setOpened] = useState(false);
-			const {
-				user,
-				error: userError,
-				isLoading,
-				mutateUser,
-			} = useUser("/", false);
+const Banner = memo(
+	forwardRef<HTMLDivElement, BannerProps>(function Banner(
+		/* eslint-disable react/prop-types */
+		{
+			iconSrc,
+			headerText,
+			previewImage,
+			previewBlurhash,
+			url,
+			category,
+			loggedInURL,
+			id,
+			isActive,
+			children,
+		},
+		/* eslint-enable react/prop-types */
+		ref,
+	) {
+		// Banners are hydrated with them being closed, less layout shift
+		const [isOpened, setOpened] = useState(false);
+		const {
+			user,
+			error: userError,
+			isLoading,
+			mutateUser,
+		} = useUser("/", false);
 
-			const link =
-				user && loggedInURL
-					? loggedInURL
-							// eslint-disable-next-line react/prop-types
-							.replace("{Profile}", user.profile.userInfo.membershipId)
-							.replace("{Clan}", user.clan?.group.groupId ?? "")
-							.replace(
-								"{Platform}",
-								user.profile.userInfo.membershipType.toString(),
-							)
-							.replace(
-								"{Platform2Code}",
-								getPlatformCode(user.profile.userInfo.membershipType, 0),
-							)
-							.replace(
-								"{Character}",
-								currentCharacter(user.characters)?.characterId ?? "",
-							)
-					: url;
+		const link =
+			user && loggedInURL
+				? loggedInURL
+						// eslint-disable-next-line react/prop-types
+						.replace("{Profile}", user.profile.userInfo.membershipId)
+						.replace("{Clan}", user.clan?.group.groupId ?? "")
+						.replace(
+							"{Platform}",
+							user.profile.userInfo.membershipType.toString(),
+						)
+						.replace(
+							"{Platform2Code}",
+							getPlatformCode(user.profile.userInfo.membershipType, 0),
+						)
+						.replace(
+							"{Character}",
+							currentCharacter(user.characters)?.characterId ?? "",
+						)
+				: url;
 
-			useEffect(() => {
-				const jsonString = localStorage.getItem("toggledBanners");
-				const map =
-					jsonString != null
-						? new Map<string, boolean>(JSON.parse(jsonString))
-						: new Map<string, boolean>();
-				// But when the page loads they are opened if there is no stored value
-				setOpened(map.get(url) ?? true);
-				return () => {};
-			}, [url]);
+		useEffect(() => {
+			const jsonString = localStorage.getItem("toggledBanners");
+			const map =
+				jsonString != null
+					? new Map<string, boolean>(JSON.parse(jsonString))
+					: new Map<string, boolean>();
+			// But when the page loads they are opened if there is no stored value
+			setOpened(map.get(url) ?? true);
+			return () => {};
+		}, [url]);
 
-			function saveState(state: boolean) {
-				const jsonString = localStorage.getItem("toggledBanners");
-				const map =
-					jsonString != null
-						? new Map<string, boolean>(JSON.parse(jsonString))
-						: new Map<string, boolean>();
-				map.set(url, state);
-				localStorage.setItem(
-					"toggledBanners",
-					JSON.stringify(Array.from(map.entries())),
-				);
-			}
+		function saveState(state: boolean) {
+			const jsonString = localStorage.getItem("toggledBanners");
+			const map =
+				jsonString != null
+					? new Map<string, boolean>(JSON.parse(jsonString))
+					: new Map<string, boolean>();
+			map.set(url, state);
+			localStorage.setItem(
+				"toggledBanners",
+				JSON.stringify(Array.from(map.entries())),
+			);
+		}
 
-			const { attributes, listeners, setNodeRef, transform, transition } =
-				useSortable({ id });
+		const { attributes, listeners, setNodeRef, transform, transition } =
+			useSortable({ id });
 
-			const style = {
-				transform: CSS.Translate.toString(transform),
-				transition,
-			};
-			return (
-				<article
-					className={clsx(
-						styles.banner,
-						isOpened ? "row-span-6" : "row-span-1",
-						isActive === true ? "opacity-0" : "opacity-100",
-					)}
-					ref={setNodeRef}
-					style={style}
-					{...attributes}
-				>
-					<div className={styles.container}>
-						<div className={styles.header}>
-							<div className="flex items-center p-2 py-4 w-14 bg-banner-dark">
-								<a
-									className="block relative w-10 h-10"
-									href={link}
-									target="_blank"
-									rel="noopener noreferrer"
-									aria-label={`${headerText} link`}
-								>
-									<Image
-										src={iconSrc}
-										objectFit="cover"
-										layout="fill"
-										alt={`${headerText} icon`}
-										unoptimized
-									/>
-								</a>
-							</div>
-
-							<h3 className={styles.headerText}>
-								<a href={link} target="_blank" rel="noopener noreferrer">
-									{headerText}
-								</a>
-							</h3>
-
-							<FaGripVertical
-								className="self-center text-2xl cursor-move"
-								{...listeners}
-							/>
-
-							<motion.div
-								className={styles.toggle}
-								animate={{
-									rotate: isOpened ? 180 : 0,
-								}}
-								transition={{
-									duration: 0.3,
-									ease: "backInOut",
-								}}
-							>
-								<button
-									type="button"
-									onClick={() => {
-										saveState(!isOpened);
-										setOpened(!isOpened);
-									}}
-									aria-label="Toggle open button"
-								>
-									<FaAngleDown className="text-5xl" />
-								</button>
-							</motion.div>
-						</div>
-
-						<div
-							style={{ height: "292px" }}
-							className={clsx(
-								// "aspect-w-16 aspect-h-9",
-								isOpened ? "block" : "hidden",
-							)}
-						>
-							<Image
-								src={previewImage}
-								objectFit="cover"
-								layout="intrinsic"
-								width={520}
-								height={292}
-								alt={`${headerText} preview`}
-								placeholder="blur"
-								blurDataURL={previewBlurhash}
-							/>
-						</div>
-
-						<figure
-							className={clsx(styles.figure, isOpened ? "block" : "hidden")}
-						>
-							<MDXRemote {...children} components={{ h4: H4 }} />
+		const style = {
+			transform: CSS.Translate.toString(transform),
+			transition,
+		};
+		return (
+			<article
+				className={clsx(
+					styles.banner,
+					isOpened ? "row-span-6" : "row-span-1",
+					isActive === true ? "opacity-0" : "opacity-100",
+				)}
+				ref={setNodeRef}
+				style={style}
+				{...attributes}
+			>
+				<div className={styles.container}>
+					<div className={styles.header}>
+						<div className="flex items-center p-2 py-4 w-14 bg-banner-dark">
 							<a
-								className={styles.button}
+								className="block relative w-10 h-10"
 								href={link}
 								target="_blank"
 								rel="noopener noreferrer"
+								aria-label={`${headerText} link`}
 							>
-								Open
+								<Image
+									src={iconSrc}
+									objectFit="cover"
+									layout="fill"
+									alt={`${headerText} icon`}
+									unoptimized
+								/>
 							</a>
-						</figure>
-					</div>
-				</article>
-			);
-		},
-	),
-);
+						</div>
 
-Banner.displayName = "Banner";
+						<h3 className={styles.headerText}>
+							<a href={link} target="_blank" rel="noopener noreferrer">
+								{headerText}
+							</a>
+						</h3>
+
+						<FaGripVertical
+							className="self-center text-2xl cursor-move"
+							{...listeners}
+						/>
+
+						<motion.div
+							className={styles.toggle}
+							animate={{
+								rotate: isOpened ? 180 : 0,
+							}}
+							transition={{
+								duration: 0.3,
+								ease: "backInOut",
+							}}
+						>
+							<button
+								type="button"
+								onClick={() => {
+									saveState(!isOpened);
+									setOpened(!isOpened);
+								}}
+								aria-label="Toggle open button"
+							>
+								<FaAngleDown className="text-5xl" />
+							</button>
+						</motion.div>
+					</div>
+
+					<div
+						style={{ height: "292px" }}
+						className={clsx(
+							// "aspect-w-16 aspect-h-9",
+							isOpened ? "block" : "hidden",
+						)}
+					>
+						<Image
+							src={previewImage}
+							objectFit="cover"
+							layout="intrinsic"
+							width={520}
+							height={292}
+							alt={`${headerText} preview`}
+							placeholder="blur"
+							blurDataURL={previewBlurhash}
+						/>
+					</div>
+
+					<figure
+						className={clsx(styles.figure, isOpened ? "block" : "hidden")}
+					>
+						<MDXRemote {...children} components={{ h4: H4 }} />
+						<a
+							className={styles.button}
+							href={link}
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Open
+						</a>
+					</figure>
+				</div>
+			</article>
+		);
+	}),
+);
 
 export default Banner;
