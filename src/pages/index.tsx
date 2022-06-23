e3a75ff28025e6713fd057b019b816b9ed922084
Andris Borbás <andris.borbas@gmail.com>
2022-06-23 22:20:57 +0200

    Reset notice

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -119,10 +119,12 @@ export default function Index({ banners, d2info }: PageProps) {
 	return (
 		<Layout className="safe-area-x relative mx-auto mb-8 flex flex-col sm:px-4 md:px-8 lg:px-12 xl:px-16">
 			<section className={styles.notices}>
-				<Notice id="notice6" className="mt-8">
+				<Notice id="notice7" className="mt-6">
 					<h2 className="mb-1 font-semibold">New features:</h2>
 					<h3>Login to have personalized links to sites.</h3>
-					<h3>Click the timer to switch between them.</h3>
+					<h3>
+						Click the countdown to switch between weekly reset and season end.
+					</h3>
 					<a
 						className="mt-2 text-sm underline decoration-yellow-300 hover:underline-offset-2"
 						href="https://ko-fi.com/andrisborbas"
