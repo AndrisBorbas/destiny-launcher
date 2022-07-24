f0aeb6b6c403a8d9bae77c59d0d79b780a86662a
Andris Borbás <andris.borbas@gmail.com>
2022-07-24 23:22:23 +0200

    Remove google tag manager, add umami

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,37 +10,26 @@ import Script from "next/script";
 import React, { useEffect } from "react";
 import { SWRConfig } from "swr";
 
-import { GTM_ID, GTMPageView } from "@/utils/gtm";
-
 export default function DLApp({ Component, pageProps }: AppProps) {
-	const router = useRouter();
-	useEffect(() => {
-		const handleRouteChange = (url: string) => GTMPageView(url);
-		router.events.on("routeChangeComplete", handleRouteChange);
-		return () => {
-			router.events.off("routeChangeComplete", handleRouteChange);
-		};
-	}, []);
+	// const router = useRouter();
+	// useEffect(() => {
+	// 	router.events.on("routeChangeComplete", handleRouteChange);
+	// 	return () => {
+	// 		router.events.off("routeChangeComplete", handleRouteChange);
+	// 	};
+	// }, []);
 
 	return (
 		<React.StrictMode>
-			{/* Google Tag Manager */}
-			<Script id="gtm" strategy="afterInteractive">
-				{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-					})(window,document,'script','dataLayer','${GTM_ID}');`}
-			</Script>
-			{/* End Google Tag Manager */}
-			{/* Google Tag Manager (noscript) */}
-			<noscript
-				// eslint-disable-next-line react/no-danger
-				dangerouslySetInnerHTML={{
-					__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-				}}
+			{/* Umami analytics */}
+			<Script
+				async
+				defer
+				data-website-id="6dbdd14e-cbac-4d0e-8231-38d54549a3e2"
+				src="http://analytics.andrisborbas.com/umami.js"
 			/>
-			{/* End Google Tag Manager (noscript) */}
+			{/* End Umami analytics */}
+
 			<SWRConfig
 				value={{ fetcher: (url) => fetch(url).then((res) => res.json()) }}
 			>
